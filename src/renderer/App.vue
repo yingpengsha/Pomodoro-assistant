514214ec92109ec691b3a74882adec47f9d56304
PengshaYing <yingfusheng@foxmail.com>
2021-12-09 04:03:15 +0800

    temp

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <n-layout has-sider>
+    <n-layout-sider
+      :native-scrollbar="false"
+      :collapsed-width="0"
+      collapse-mode="transform"
+      trigger-style="top: 240px;"
+      collapsed-trigger-style="top: 240px; right: -20px;"
+      bordered
+      show-trigger="arrow-circle"
+    >
+      时钟列表
+    </n-layout-sider>
+    <n-layout-content>
+      当前进度
+    </n-layout-content>
+  </n-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+<script setup lang="ts">
+import { NLayout, NLayoutSider, NLayoutContent } from 'naive-ui'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/renderer/constants/clocks.ts
@@ -0,0 +1,41 @@
+export interface ClockType {
+  name: string
+  key: string
+  color: string
+  minTime: number
+}
+
+const Clocks: ClockType[] = [
+  {
+    name: '普通',
+    key: 'normal',
+    color: '#f5222d',
+    minTime: 25
+  },
+  {
+    name: '读书',
+    key: 'read',
+    color: '#52c41a',
+    minTime: 25
+  },
+  {
+    name: '学习',
+    key: 'study',
+    color: '#1890ff',
+    minTime: 25
+  },
+  {
+    name: '代码',
+    key: 'code',
+    color: '#722ed1',
+    minTime: 25
+  },
+  {
+    name: '冥想',
+    key: 'meditation',
+    color: '#d9d9d9',
+    minTime: 10
+  }
+]
+
+export default Clocks
