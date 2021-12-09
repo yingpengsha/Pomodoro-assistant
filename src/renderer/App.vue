a9f93c2e693bfca6d6a0b34bd039dfe88d751d4d
Pengsha Ying <yingfusheng@foxmail.com>
2021-12-09 15:26:14 +0800

    dependencies change

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,12 @@
 <template>
   <n-layout has-sider>
     <n-layout-sider
+      bordered
       :native-scrollbar="false"
       :collapsed-width="0"
       collapse-mode="transform"
-      trigger-style="top: 240px;"
-      collapsed-trigger-style="top: 240px; right: -20px;"
-      bordered
+      trigger-style="top: 240px; width: 40px; height: 40px;"
+      collapsed-trigger-style="top: 240px; right: -30px; width: 40px; height: 40px;"
       show-trigger="arrow-circle"
     >
       时钟列表
